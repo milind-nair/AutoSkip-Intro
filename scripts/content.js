7260c5ed378880158d9dfd5bea1f3ebbada563cd
milind-nair <nairmilind3@gmail.com>
2023-10-26 18:47:27 +0530

    added support for Hotstar

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,18 @@
-function clickSkipIntro(entries) {
+const currentURL = window.location.href;
+const urlParts = currentURL.split(".");
+let query = "";
+const provider = urlParts[1];
+if (provider === "netflix") {
+  query = 'button[data-uia="player-skip-intro"]';
+} else if (provider === "hotstar") {
+  query =
+    'button[class="_1CSTLo7uotP5mTlp3jKun7 _2HYkSYMlyfm2mRvVj37UvB _1hvr__NMJbQHNb3s6iYa1w BODY2_MEDIUM TEXT_COLOR_L0"]';
+}
+
+function clickSkipIntro(entries, introButtonClassTag) {
   entries.forEach((entry) => {
     entry.addedNodes.forEach((node) => {
-      const skipIntroButton = document.querySelector(
-        'button[data-uia="player-skip-intro"]'
-      );
-
+      const skipIntroButton = document.querySelector(query);
       if (skipIntroButton) {
         try {
           skipIntroButton.click();
@@ -15,7 +23,7 @@ function clickSkipIntro(entries) {
     });
   });
 }
-
+console.log(urlParts[1]);
 const observer = new MutationObserver(clickSkipIntro);
 const observerOptions = {
   subtree: true,
